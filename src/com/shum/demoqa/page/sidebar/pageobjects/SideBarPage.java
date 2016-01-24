e878527e29eea72a6bbe031f2a0e780f84c46a93
trinetixishum <igor.shumeiko@trinetix.com>
2016-01-24 22:58:10 +0200

    SideBar class and package added

---
+++ src/com/shum/demoqa/page/sidebar/pageobjects/SideBarPage.java
@@ -0,0 +1,11 @@
+package com.shum.demoqa.page.sidebar.pageobjects;
+
+import com.shum.demoqa.page.AbstractPage;
+
+public class SideBarPage extends AbstractPage {
+
+	public SideBarPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
