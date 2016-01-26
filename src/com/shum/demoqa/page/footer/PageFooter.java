894335918c2a51779f5a25267e0506662eea94b5
Izek <izek.schum@gmail.com>
2016-01-27 00:51:23 +0200

    Added exeption in PageFooterConstructor

--- src/com/shum/demoqa/page/footer/PageFooter.java
+++ src/com/shum/demoqa/page/footer/PageFooter.java
@@ -4,19 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import com.shum.demoqa.exceptions.ElementNotFoundException;
 import com.shum.demoqa.page.AbstractPage;
 
 public class PageFooter extends AbstractPage {
 
-	//TODO: Add PageFooter to each page
-	
 	public PageFooter(WebDriver wDriver) {
 		this.wDriver = wDriver;
+
+		if (!isElementAppeared(footerHeader, 5)) {
+			throw new ElementNotFoundException("'Footer' is not present on current page!..");
+		}
 	}
 
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]//div/aside/h3[text()=\"About Us\"]")
 	WebElement footerHeader;
-	
+
 	public boolean isFooterOnPage() {
 		return isElementAppeared(footerHeader, 5);
 	}
