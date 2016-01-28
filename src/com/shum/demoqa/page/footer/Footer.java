b979830ddb70b175e73218961ad298ac2f64c3b0
Izek <izek.schum@gmail.com>
2016-01-28 23:34:57 +0200

    Added class modifiers for Footer WebElements. minor code cleanup

--- src/com/shum/demoqa/page/footer/Footer.java
+++ src/com/shum/demoqa/page/footer/Footer.java
@@ -14,37 +14,33 @@ public class Footer extends AbstractPage {
 	}
 
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]//div/aside/h3[text()=\"About Us\"]")
-	WebElement footerHeader;
+	public static WebElement footerHeader;
 
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"container\"]/div//aside/div[@class=\"textwidget\"]")
-	WebElement footerDescription;
+	public static WebElement footerDescription;
 
 	// Baseline Locators
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"site-info\"]/p")
-	WebElement footerBaseLine;
+	public static WebElement footerBaseLine;
 
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"site-info\"]/p/a[@href=\"http://toolsqa.com/\"]")
-	WebElement toolsQALink;
+	public static WebElement toolsQALink;
 
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"site-info\"]/p/a[@href=\"http://oneiricindia.com/\"]")
-	WebElement oneiricTechLink;
+	public static WebElement oneiricTechLink;
 
 	// SocialMedia links
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div/div/div/aside/ul/li[@class=\"facebook\"]/a/i")
-	WebElement facebookLink;
+	public static WebElement facebookLink;
 
 	@FindBy(xpath = " // /html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div/div/div/aside/ul/li[@class=\"twitter\"]/a/i")
-	WebElement twitterLink;
+	public static WebElement twitterLink;
 
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div/div/div/aside/ul/li[@class=\"gplus\"]/a/i")
-	WebElement googlePlusLink;
+	public static WebElement googlePlusLink;
 
 	public boolean isFooterOnPage() {
 		return isElementAppeared(footerHeader, 5);
-	}
-
-	public WebElement getTwiLink() {
-		return twitterLink;
 	}
 
 	public void focusOnElement(WebElement webElement) {
--- src/com/shum/demoqa/tests/footer/basictests/PageFooterTest.java
+++ src/com/shum/demoqa/tests/footer/basictests/PageFooterTest.java
@@ -27,18 +27,18 @@ public class PageFooterTest extends BaseTest {
 		WebPage homePage = new WebPage(wDriver);
 
 		homePage.navBar.openHomePage().getFooter().isFooterOnPage();
-	
+
 		Footer pFoot = new Footer(wDriver);
-		
-		//homePage.navBar.openHomePage().pageFooter.focusOnElement(pFoot.getTwiLink());
-		
+
+		// homePage.navBar.openHomePage().pageFooter.focusOnElement(pFoot.getTwiLink());
+
 		homePage.navBar.openHomePage();
-		
-		WebElement aboutTabButton = wDriver.findElement(By.xpath("/html/body/div[@id=\"page\"]//ul[@id=\"menu-primary-menu\"]//a[@title=\"About us\"]"));
+
+		WebElement aboutTabButton = wDriver.findElement(
+				By.xpath("/html/body/div[@id=\"page\"]//ul[@id=\"menu-primary-menu\"]//a[@title=\"About us\"]"));
 
 		focusOnElement(aboutTabButton);
-		
-		
+
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
@@ -46,38 +46,46 @@ public class PageFooterTest extends BaseTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void openToolTip() {
-		
+
 		WebPage homePage = new WebPage(wDriver);
-		
+
 		homePage.navBar.openHomePage().getFooter().isFooterOnPage();
-		
-		WebElement aboutTabButton = wDriver.findElement(By.xpath("/html/body/div[@id=\"page\"]//ul[@id=\"menu-primary-menu\"]//a[@title=\"About us\"]"));
+
+		WebElement aboutTabButton = wDriver.findElement(
+				By.xpath("/html/body/div[@id=\"page\"]//ul[@id=\"menu-primary-menu\"]//a[@title=\"About us\"]"));
 
 		String titleText = aboutTabButton.toString();
+
+		System.out.println(titleText);
 		
 		focusOnElement(aboutTabButton);
-		
+
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Test");
-		
-		
+
+	
 		WebElement wEle = wDriver.findElement(By.cssSelector("li.facebook a.bstooltip"));
+		homePage.footer.focusOnElement(wEle);
+		
 		
 		System.out.println(wEle.getText());
+		
+		homePage.isElementAppeared(Footer.footerHeader, 5);
+		
 	}
-	
+
 	public void focusOnElement(WebElement webElement) {
 		new Actions(wDriver).moveToElement(webElement).perform();
-		
+
 	
 		try {
 			Thread.sleep(5000);
@@ -86,5 +94,5 @@ public class PageFooterTest extends BaseTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
