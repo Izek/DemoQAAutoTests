55443fd5d8c706ef4bb00c42ef8ce6c0eaafe64a
trinetixishum <igor.shumeiko@trinetix.com>
2016-01-28 16:46:46 +0200

    Assigned Footer Elements locators.

--- src/com/shum/demoqa/page/footer/Footer.java
+++ src/com/shum/demoqa/page/footer/Footer.java
@@ -19,24 +19,24 @@ public class Footer extends AbstractPage {
 	WebElement footerDescription;
 
 	// Baseline Locators
-	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div[@class="site-info"]/p
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"site-info\"]/p")
 	WebElement footerBaseLine;
 
-	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div[@class="site-info"]/p/a[@href="http://toolsqa.com/"]
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"site-info\"]/p/a[@href=\"http://toolsqa.com/\"]")
 	WebElement toolsQALink;
 
-	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div[@class="site-info"]/p/a[@href="http://oneiricindia.com/"]
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"site-info\"]/p/a[@href=\"http://oneiricindia.com/\"]")
 	WebElement oneiricTechLink;
 
 	// SocialMedia links
-	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div/div/div/aside/ul/li[@class="facebook"]/a/i
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div/div/div/aside/ul/li[@class=\"facebook\"]/a/i")
 	WebElement facebookLink;
 
-	@FindBy(xpath = "")
-	WebElement twitterLink; // /html/body/div[@id="page"]/footer[@id="colophon"]/div/div/div/aside/ul/li[@class="twitter"]/a/i
+	@FindBy(xpath = " // /html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div/div/div/aside/ul/li[@class=\"twitter\"]/a/i")
+	WebElement twitterLink;
 
-	@FindBy(xpath = "")
-	WebElement googlePlusLink; // /html/body/div[@id="page"]/footer[@id="colophon"]/div/div/div/aside/ul/li[@class="gplus"]/a/i
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div/div/div/aside/ul/li[@class=\"gplus\"]/a/i")
+	WebElement googlePlusLink; 
 
 	public boolean isFooterOnPage() {
 		return isElementAppeared(footerHeader, 5);
