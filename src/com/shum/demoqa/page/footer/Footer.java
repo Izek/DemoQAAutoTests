b58a6365f571beb82f98537e182c52f2c1603731
trinetixishum <igor.shumeiko@trinetix.com>
2016-01-28 16:39:07 +0200

    Added Footer elements locators

--- src/com/shum/demoqa/page/footer/Footer.java
+++ src/com/shum/demoqa/page/footer/Footer.java
@@ -15,6 +15,29 @@ public class Footer extends AbstractPage {
 	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]//div/aside/h3[text()=\"About Us\"]")
 	WebElement footerHeader;
 
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/footer[@id=\"colophon\"]/div[@class=\"container\"]/div//aside/div[@class=\"textwidget\"]")
+	WebElement footerDescription;
+
+	// Baseline Locators
+	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div[@class="site-info"]/p
+	WebElement footerBaseLine;
+
+	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div[@class="site-info"]/p/a[@href="http://toolsqa.com/"]
+	WebElement toolsQALink;
+
+	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div[@class="site-info"]/p/a[@href="http://oneiricindia.com/"]
+	WebElement oneiricTechLink;
+
+	// SocialMedia links
+	@FindBy(xpath = "") // /html/body/div[@id="page"]/footer[@id="colophon"]/div/div/div/aside/ul/li[@class="facebook"]/a/i
+	WebElement facebookLink;
+
+	@FindBy(xpath = "")
+	WebElement twitterLink; // /html/body/div[@id="page"]/footer[@id="colophon"]/div/div/div/aside/ul/li[@class="twitter"]/a/i
+
+	@FindBy(xpath = "")
+	WebElement googlePlusLink; // /html/body/div[@id="page"]/footer[@id="colophon"]/div/div/div/aside/ul/li[@class="gplus"]/a/i
+
 	public boolean isFooterOnPage() {
 		return isElementAppeared(footerHeader, 5);
 	}
