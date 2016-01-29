c8f15525879f15beb955625985b5c4981df1d292
trinetixishum <igor.shumeiko@trinetix.com>
2016-01-29 15:38:51 +0200

    Basic Footer Tests for HomePage are implemented.

--- src/com/shum/demoqa/test/footer/data/FooterTestData.java
+++ src/com/shum/demoqa/test/footer/data/FooterTestData.java
@@ -8,4 +8,19 @@ package com.shum.demoqa.test.footer.data;
  */
 public interface FooterTestData {
 
+	public static final String FOOTER_DESCTPTION_TEXT = "Selenium is a software testing framework for the web that facilitates the automation of browsers. "
+			+ "The Selenium project produces various tools for automation testing such as Selenium IDE, "
+			+ "Selenium Remote Control (RC), Selenium Grid and Selenium 2.0 & WebDriver. "
+			+ "Learning all the tools will give you many different options for approaching different "
+			+ "automation problems. The entire suits of tools result in a rich set of testing functions "
+			+ "specially geared to the needs of testing of web application of all types.";
+
+	// 'Tools QA' link
+	public static final String FOOTER_TOOLSQA_HOMEPAGE_HEADER = "TOOLSQA | Tutorials on Selenium, Java, TestNG, Cucumber, Spec Flow, Maven, IBATIS, Appium, Mobile AUtomation";
+	public static final String FOOTER_TOOLSQA_HOMEPAGE_ADDRESS = "http://toolsqa.com/";
+
+	// 'OneiricTech' link
+	public static final String FOOTER_ONEIRIC_HOMEPAGE_HEADER = "Powered by Oneiric Tech Solutions Pvt. Ltd.";
+	public static final String FOOTER_ONEIRIC_HOMEPAGE_ADDRESS = "http://www.oneiricindia.com/";
+
 }
