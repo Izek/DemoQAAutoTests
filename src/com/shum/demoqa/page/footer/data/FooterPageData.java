52d488c10c2be36d25eb675312d2a71c5568e7c7
trinetixishum <igor.shumeiko@trinetix.com>
2016-01-29 13:23:30 +0200

    Added Interfaces for Footer and FooterTests classes.

---
+++ src/com/shum/demoqa/page/footer/data/FooterPageData.java
@@ -0,0 +1,11 @@
+package com.shum.demoqa.page.footer.data;
+
+/**
+ * <code>FooterPageData</code> interface is intended to store static final
+ * values used for <code>Footer</code> class.
+ * 
+ * @author shum
+ */
+public interface FooterPageData {
+
+}
---
+++ src/com/shum/demoqa/test/footer/data/FooterTestData.java
@@ -0,0 +1,11 @@
+package com.shum.demoqa.test.footer.data;
+
+/**
+ * <code>FooterTestData</code> interface is intended to store static final
+ * values used for testing classes.
+ * 
+ * @author shum
+ */
+public interface FooterTestData {
+
+}
