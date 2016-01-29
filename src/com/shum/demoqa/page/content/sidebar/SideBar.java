1a006915501343b11f198c405aa1b2e03e7c16c0
trinetixishum <igor.shumeiko@trinetix.com>
2016-01-30 01:07:53 +0200

    SideBar page links locators added.

--- src/com/shum/demoqa/page/content/sidebar/SideBar.java
+++ src/com/shum/demoqa/page/content/sidebar/SideBar.java
@@ -14,10 +14,49 @@ public class SideBar extends AbstractPage {
 		this.wDriver = wDriver;
 	}
 
-	@FindBy(xpath = "/html/body/div/div/div[@id=\"secondary\"]/aside[@id=\"nav_menu-6\"]/div/h3[text()=\"Registration\"]")
+	// 'Registration' block elements
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div/h3[text()=\"Registration\"]")
 	WebElement registrationSBHeader;
-	@FindBy(xpath = "/html/body/div/div/div[@id=\"secondary\"]/aside[@id=\"nav_menu-6\"]/div/div/ul/li/a[text()=\"Registration\"]")
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div/div/ul/li/a[text()=\"Registration\"]")
 	WebElement registrationSBLink;
+
+	// 'Integration' block elements
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div/h3[text()=\"interaction\"]")
+	WebElement interactionHeader;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Draggable\"]")
+	WebElement draggableLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Droppable\"]")
+	WebElement droppableLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Resizable\"]")
+	WebElement resizableLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Selectable\"]")
+	WebElement selectableLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Sortable\"]")
+	WebElement sortableLink;
+
+	// 'Widget' block elements
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div/h3[text()=\"Widget\"]")
+	WebElement widgetHeader;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Accordion\"]")
+	WebElement accordionLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Autocomplete\"]")
+	WebElement autocompleteLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Datepicker\"]")
+	WebElement datepickerLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Menu\"]")
+	WebElement menuLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Slider\"]")
+	WebElement sliderLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Tabs\"]")
+	WebElement tabsLink;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Tooltip\"]")
+	WebElement tooltipLink;
+
+	// 'Frames and Windows' block elements
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div/h3[text()=\"Frames and Windows\"]")
+	WebElement framesAndWindowsHeader;
+	@FindBy(xpath = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"secondary\"]/aside/div//ul/li/a[text()=\"Frames and windows\"]")
+	WebElement framesAndWindowsLink;
 
 	public boolean isSideBarPresentOnPage() {
 		return isElementAppeared(registrationSBHeader, 5);
