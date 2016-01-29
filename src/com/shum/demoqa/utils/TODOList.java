dbad3839f7bc102098de6d3a080d9a221f8ee3cc
trinetixishum <igor.shumeiko@trinetix.com>
2016-01-29 15:41:23 +0200

    Added TODO List class. Temporary.

---
+++ src/com/shum/demoqa/utils/TODOList.java
@@ -0,0 +1,11 @@
+package com.shum.demoqa.utils;
+
+public class TODOList {
+
+	// TODO: Implement waitFor(int milliseconds) method in AbstractPage.class,
+	// which should wait given time in milliseconds.
+
+	// TODO: Create documentation for Footer.class
+	// TODO: Create documentation for FooterTest.class
+
+}
