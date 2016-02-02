207532ba76670d02c186c27dab40a3f4e34c17dc
Izek <izek.schum@gmail.com>
2016-02-03 01:02:24 +0200

    HomePage.class refactored.

--- src/com/shum/demoqa/page/navbar/data/homepage/HomePageData.java
+++ src/com/shum/demoqa/page/navbar/data/homepage/HomePageData.java
@@ -2,4 +2,11 @@ package com.shum.demoqa.page.navbar.data.homepage;
 
 public interface HomePageData {
 
+	// public final static String HOME_PAGE_HEADER =
+	// "/html/body//main/article/header/h1[@class=\"entry-title\"][text()=\"Home\"]";
+
+	// TODO: Temp locator
+	// /html/body/div[@id="page"]/div[@id="content"]/div[@id="primary"]/main[@id="main"]/article
+
+	public final static String HOME_PAGE_HEADER_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/header/h1";
 }
