da5fd16a054f17076abf5d0dbd9861fff1f59141
Izek <izek.schum@gmail.com>
2016-02-03 10:00:41 +0200

    working on HomePage locators.

--- src/com/shum/demoqa/page/navbar/data/homepage/HomePageData.java
+++ src/com/shum/demoqa/page/navbar/data/homepage/HomePageData.java
@@ -9,4 +9,12 @@ public interface HomePageData {
 	// /html/body/div[@id="page"]/div[@id="content"]/div[@id="primary"]/main[@id="main"]/article
 
 	public final static String HOME_PAGE_HEADER_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/header/h1";
+
+	public final static String UNIQUE_AND_CLEAN_IMG_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div/p/a";
+	public final static String CUSTOMER_SUPPORT_IMG_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div/p/i/a";
+	public final static String VERY_FLEXIBLE_IMG_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div/i/a";
+
+	// /html/body/div[@id="page"]/div[@id="content"]/div[@id="primary"]/main[@id="main"]/article/div/div/div[@id="tabs222"]
+	
+	
 }
