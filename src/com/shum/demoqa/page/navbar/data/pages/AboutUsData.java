fb52ba6db46878a2b82d68f13da7feaf1bc67cce
trinetixishum <igor.shumeiko@trinetix.com>
2016-02-04 15:21:38 +0200

    AboutUs page. Article locators added.

--- src/com/shum/demoqa/page/navbar/data/pages/AboutUsData.java
+++ src/com/shum/demoqa/page/navbar/data/pages/AboutUsData.java
@@ -1,5 +1,6 @@
 package com.shum.demoqa.page.navbar.data.pages;
 
 public interface AboutUsData {
-
+	public final static String ABOUT_US_PAGE_HEADER = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/header/h1";
+	public final static String ABOUT_US_PAGE_CONTENT = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/p";
 }
