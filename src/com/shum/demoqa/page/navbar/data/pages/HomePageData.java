fda2b34edcfc72a22c850468d4834917477e51fb
trinetixishum <igor.shumeiko@trinetix.com>
2016-02-04 19:53:48 +0200

    HomePageData locators to the TabBar added.

--- src/com/shum/demoqa/page/navbar/data/pages/HomePageData.java
+++ src/com/shum/demoqa/page/navbar/data/pages/HomePageData.java
@@ -12,13 +12,18 @@ public interface HomePageData {
 
 	public final static String HOME_PAGE_UNIQUE_AND_CLEAN_IMG_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div/p/a";
 	public final static String HOME_PAGE_CUSTOMER_SUPPORT_IMG_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div/p/i/a";
-	public final static String HOME_PAGE_VERY_FLEXIBLE_IMG_XPATH    = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div/i/a";
+	public final static String HOME_PAGE_VERY_FLEXIBLE_IMG_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div/i/a";
 
 	//
 	public final static String HOME_PAGE_UNIQUE_AND_CLEAN_DESCR_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div[1]/div/p[2]";
 	public final static String HOME_PAGE_CUSTOMER_SUPPORT_DESCR_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div[2]/div/p[2]";
-	public final static String HOME_PAGE_VERY_FLEXIBLE_DESCR_XPATH    = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div[3]/div/p[2]";
+	public final static String HOME_PAGE_VERY_FLEXIBLE_DESCR_XPATH = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div[3]/div/p[2]";
 
 	// /html/body/div[@id="page"]/div[@id="content"]/div[@id="primary"]/main[@id="main"]/article/div/div/div[@id="tabs222"]
 
+	//
+	public final static String HOME_PAGE_TABBAR_ACTIVE_TAB = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div[@id=\"tabs222\"]/ul/li[@aria-selected=\"true\"]/a";
+	public final static String HOME_PAGE_TABBAR_INACTIVE_TAB = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div[@id=\"tabs222\"]/ul/li[@aria-selected=\"false\"]/a";
+
+	public final static String HOME_PAGE_TABBAR_HOVERED_TAB = "/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div[@id=\"tabs222\"]/ul/li[contains(@class, 'ui-state-hover')]";
 }
