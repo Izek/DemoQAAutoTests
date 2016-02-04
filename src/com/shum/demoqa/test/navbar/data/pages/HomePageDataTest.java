703c77678e8d4d4034ad11ef573e1a12ed2982cf
Izek <izek.schum@gmail.com>
2016-02-04 23:17:59 +0200

    HomePageDataTest - added data.

--- src/com/shum/demoqa/test/navbar/data/pages/HomePageDataTest.java
+++ src/com/shum/demoqa/test/navbar/data/pages/HomePageDataTest.java
@@ -8,4 +8,26 @@ public interface HomePageDataTest {
 	public final static String HOME_PAGE_UNIQUE_AND_CLEAN_DESCR_TEXT = "Aliquam hendrit rutrum iaculis nullam ondimentum maluada velit beum donec sit amet";
 	public final static String HOME_PAGE_CUSTOMER_SUPPORT_DESCR_TEXT = "Aliquam hendrit rutrum iaculis nullam ondimentum maluada velit beum donec sit amet";
 	public final static String HOME_PAGE_VERY_FLEXIBLE_DESCR_TEXT = "Aliquam hendrit rutrum iaculis nullam ondimentum maluada velit beum donec sit amet";
+
+	// TabBar Names
+	public final static String HOME_PAGE_TABS_TAB_TITLE_ONE = "Tab 1";
+	public final static String HOME_PAGE_TABS_TAB_TITLE_TWO = "Tab 2";
+	public final static String HOME_PAGE_TABS_TAB_TITLE_THREE = "Tab 3";
+	public final static String HOME_PAGE_TABS_TAB_TITLE_FOUR = "Tab 4";
+	public final static String HOME_PAGE_TABS_TAB_TITLE_FIVE = "Tab 5";
+
+	// TabBar Titles
+	public final static String HOME_PAGE_TABS_CONTENT_TITLE_ONE = "Content 1 Title";
+	public final static String HOME_PAGE_TABS_CONTENT_TITLE_TWO = "Content 2 Title";
+	public final static String HOME_PAGE_TABS_CONTENT_TITLE_THREE = "Content 3 Title";
+	public final static String HOME_PAGE_TABS_CONTENT_TITLE_FOUR = "Content 4 Title";
+	public final static String HOME_PAGE_TABS_CONTENT_TITLE_FIVE = "Content 5 Title";
+
+	// TabBar Content
+	public final static String HOME_PAGE_TABS_CONTENT_TEXT_ONE = "The content could contain anything text page or submit form or any other HTML objects.";
+	public final static String HOME_PAGE_TABS_CONTENT_TEXT_TWO = "The content could contain anything text page or submit form or any other HTML objects.";
+	public final static String HOME_PAGE_TABS_CONTENT_TEXT_THREE = "The content could contain anything text page or submit form or any other HTML objects.";
+	public final static String HOME_PAGE_TABS_CONTENT_TEXT_FOUR = "The content could contain anything text page or submit form or any other HTML objects.";
+	public final static String HOME_PAGE_TABS_CONTENT_TEXT_FIVE = "The content could contain anything text page or submit form or any other HTML objects.";
+
 }
