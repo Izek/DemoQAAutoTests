10481e9d69803f3a03a85477be9754c1d797acd3
Izek <izek.schum@gmail.com>
2016-02-10 13:32:16 +0200

    working

--- src/com/shum/demoqa/test/navbar/pages/HomePageTabsTest.java
+++ src/com/shum/demoqa/test/navbar/pages/HomePageTabsTest.java
@@ -1,9 +1,12 @@
 package com.shum.demoqa.test.navbar.pages;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import com.shum.demoqa.page.WebPage;
 import com.shum.demoqa.page.navbar.pages.HomePage;
+import com.shum.demoqa.test.navbar.data.pages.HomePageDataTest;
 import com.shum.demoqa.tests.BaseTest;
 
 public class HomePageTabsTest extends BaseTest {
@@ -24,16 +27,20 @@ public class HomePageTabsTest extends BaseTest {
 		wPage.navBar.openHomePage();
 
 		HomePage hPage = new HomePage(wDriver);
-		
+
 		hPage.clickOnTabOne();
 		hPage.clickOnTabTwo();
 		hPage.clickOnTabThree();
 		hPage.clickOnTabFour();
 		hPage.clickOnTabFive();
-		
 
-		
-		waitForTime(2000);
-		
+		waitForTime(500);
+
+		WebElement tHeaderOne = wDriver.findElement(By.xpath(
+				"/html/body/div[@id=\"page\"]/div[@id=\"content\"]/div[@id=\"primary\"]/main[@id=\"main\"]/article/div/div/div[@id=\"tabs222\"]/div[@id=\"tabs-4\"]"));
+		System.out.println(tHeaderOne.getAttribute("aria-hidden"));
+
+		hPage.clickOnTabFour();
+		System.out.println(tHeaderOne.getAttribute("aria-hidden"));
 	}
 }
