457e78fcb30f0c9b67fdd698709eb33665ee3dff
trinetixishum <igor.shumeiko@trinetix.com>
2016-02-12 16:55:41 +0200

    Added Test data to DraggablePage

--- src/com/shum/demoqa/test/navbar/data/pages/DraggablePageDataTest.java
+++ src/com/shum/demoqa/test/navbar/data/pages/DraggablePageDataTest.java
@@ -1,5 +1,6 @@
 package com.shum.demoqa.test.navbar.data.pages;
 
 public interface DraggablePageDataTest {
-
+	public final static String DRAGGABLE_PAGE_HEADER = "Draggable";
+	public final static String DRAGGABLE_PAGE_TEXT = "Allow elements to be moved using the mouse.";
 }
