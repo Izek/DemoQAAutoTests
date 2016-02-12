75e0ca1466e3bdf5f8dd191777246deb85c505f9
trinetixishum <igor.shumeiko@trinetix.com>
2016-02-12 16:17:21 +0200

    DraggablePage classes added.

---
+++ src/com/shum/demoqa/test/navbar/pages/DraggablePageElementsTest.java
@@ -0,0 +1,11 @@
+package com.shum.demoqa.test.navbar.pages;
+
+import com.shum.demoqa.tests.BaseTest;
+
+public class DraggablePageElementsTest extends BaseTest {
+
+	public DraggablePageElementsTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
