27a28e3eaf6270fc2c0ed77f1e95d97e5b61cca4
Izek <izek.schum@gmail.com>
2016-02-24 01:41:45 +0200

    ContactPage classes added.

---
+++ src/com/shum/demoqa/test/navbar/pages/ContactPageElementTest.java
@@ -0,0 +1,11 @@
+package com.shum.demoqa.test.navbar.pages;
+
+import com.shum.demoqa.tests.BaseTest;
+
+public class ContactPageElementTest extends BaseTest {
+
+	public ContactPageElementTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
