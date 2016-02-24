7e82dcb6321943da99c6afb74583bee0c8861de2
Izek <izek.schum@gmail.com>
2016-02-24 22:52:52 +0200

    Added locators to AdminPageData interface

--- src/com/shum/demoqa/page/navbar/pages/blog/data/AdminPageData.java
+++ src/com/shum/demoqa/page/navbar/pages/blog/data/AdminPageData.java
@@ -2,4 +2,28 @@ package com.shum.demoqa.page.navbar.pages.blog.data;
 
 public interface AdminPageData {
 
+	public final static String ADMIN_PAGE_SAMPLE_POST_HEADER_NAME_XPATH = "/html/body/div/div/section/main/header/h1/span";
+
+	// Spample Post One
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_ONE_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/header/h2/";
+	public final static String ADMIN_PAGE_SAMPLE_POST_ONE_POSTED_ON_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/header/div/span/a";
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_ONE_IMAGE_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/div/a/img";
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_ONE_DESCR_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/p";
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_ONE_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/footer/p/a[contains(text(), \"Read more\")]";
+
+	// Spample Post Two
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_TW0_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/header/h2/";
+	public final static String ADMIN_PAGE_SAMPLE_POST_TW0_POSTED_ON_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/header/div/span/a";
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_TWO_IMAGE_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/a/img";
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_TWO_DESCR_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/p";
+
+	public final static String ADMIN_PAGE_SAMPLE_POST_TW0_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/footer/p/a[contains(text(), \"Read more\")]";
+
 }
