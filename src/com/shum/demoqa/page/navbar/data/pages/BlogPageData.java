33be4f6129f76442a7531539a1ca893fba7e79f2
trinetixishum <igor.shumeiko@trinetix.com>
2016-02-24 15:48:38 +0200

    Added locators to BlogPage

--- src/com/shum/demoqa/page/navbar/data/pages/BlogPageData.java
+++ src/com/shum/demoqa/page/navbar/data/pages/BlogPageData.java
@@ -2,4 +2,24 @@ package com.shum.demoqa.page.navbar.data.pages;
 
 public interface BlogPageData {
 
+	// Spample Post One
+
+	public final static String BLOG_PAGE_SAMPLE_POST_ONE_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/header/h2/";
+	public final static String BLOG_PAGE_SAMPLE_POST_ONE_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/footer/p/a[contains(text(), \"Read more\")]";
+	public final static String BLOG_PAGE_SAMPLE_POST_ONE_POSTED_ON_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/header/div/span/a";
+
+	public final static String BLOG_PAGE_SAMPLE_POST_ONE_IMAGE_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/div/a/img";
+
+	public final static String BLOG_PAGE_SAMPLE_POST_ONE_DESCR_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/p";
+
+	// Spample Post Two
+
+	public final static String BLOG_PAGE_SAMPLE_POST_TW0_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/header/h2/";
+	public final static String BLOG_PAGE_SAMPLE_POST_TW0_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/footer/p/a[contains(text(), \"Read more\")]";
+	public final static String BLOG_PAGE_SAMPLE_POST_TW0_POSTED_ON_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/header/div/span/a";
+
+	public final static String BLOG_PAGE_SAMPLE_POST_TWO_IMAGE_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/a/img";
+
+	public final static String BLOG_PAGE_SAMPLE_POST_TWO_DESCR_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/p";
+
 }
