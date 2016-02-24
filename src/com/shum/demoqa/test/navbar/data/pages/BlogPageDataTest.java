48dc16c59166224c4c7b9a5828ab9cad05d0cd58
Izek <izek.schum@gmail.com>
2016-02-24 23:12:55 +0200

    Test data added for BlogPage

--- src/com/shum/demoqa/test/navbar/data/pages/BlogPageDataTest.java
+++ src/com/shum/demoqa/test/navbar/data/pages/BlogPageDataTest.java
@@ -4,7 +4,10 @@ public interface BlogPageDataTest {
 
 	// "Blog | Demoqa";
 	public final static String BLOG_PAGE_TITLE = "Blog | Demoqa | Just another WordPress site";
-	
+
+	public final static String BLOG_PAGE_POST_ONE_HEADER_TEXT = "Sample Post";
 	public final static String BLOG_PAGE_POST_ONE_DESCRIPTION = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using ‘lorem ipsum’ is that it has a more-or-less normal distribution of letters, as opposed to using ‘This here is content’ or ‘Osama Bin Laden wanted dead";
+
+	public final static String BLOG_PAGE_POST_TWO_HEADER_TEXT = "Sample Post2";
 	public final static String BLOG_PAGE_POST_TWO_DESCRIPTION = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using ‘lorem ipsum’ is that it has a more-or-less normal distribution of letters, as opposed to using ‘This here is content’ or ‘Osama Bin Laden wanted dead";
 }
