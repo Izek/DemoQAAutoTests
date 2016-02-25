cb60310e66236d1ef33198a7c9598ed08d37544d
Izek <izek.schum@gmail.com>
2016-02-26 00:07:59 +0200

    Added several tests to the BlogPageTests

--- src/com/shum/demoqa/page/navbar/data/pages/BlogPageData.java
+++ src/com/shum/demoqa/page/navbar/data/pages/BlogPageData.java
@@ -3,21 +3,21 @@ package com.shum.demoqa.page.navbar.data.pages;
 public interface BlogPageData {
 
 	// Spample Post One element locators
-	public final static String BLOG_PAGE_SAMPLE_POST_ONE_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/header/h2/";
+	public final static String BLOG_PAGE_SAMPLE_POST_ONE_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/header/h2";
 	public final static String BLOG_PAGE_SAMPLE_POST_ONE_POSTED_ON_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/header/div/span/a";
 
 	public final static String BLOG_PAGE_SAMPLE_POST_ONE_IMAGE_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/div/a/img";
 	public final static String BLOG_PAGE_SAMPLE_POST_ONE_DESCR_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/p";
 
-	public final static String BLOG_PAGE_SAMPLE_POST_ONE_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/footer/p/a[contains(text(), \"Read more\")]";
+	public final static String BLOG_PAGE_SAMPLE_POST_ONE_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-377\"]/footer/p/a[@class=\"btn btn-primary\"][contains(text(), \"Read more\")]";
 
 	// Spample Post Two element locators
-	public final static String BLOG_PAGE_SAMPLE_POST_TWO_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/header/h2/";
+	public final static String BLOG_PAGE_SAMPLE_POST_TWO_HEADER_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/header/h2";
 	public final static String BLOG_PAGE_SAMPLE_POST_TWO_POSTED_ON_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/header/div/span/a";
 
 	public final static String BLOG_PAGE_SAMPLE_POST_TWO_IMAGE_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/a/img";
 	public final static String BLOG_PAGE_SAMPLE_POST_TWO_DESCR_XPATH = "/html/body//main/article[@id=\"post-379\"]/div/p";
 
-	public final static String BLOG_PAGE_SAMPLE_POST_TWO_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/footer/p/a[contains(text(), \"Read more\")]";
+	public final static String BLOG_PAGE_SAMPLE_POST_TWO_READ_MORE_LINK_XPATH = "/html/body//main/article[@id=\"post-379\"]/footer/p/a[@class=\"btn btn-primary\"][contains(text(), \"Read more\")]";
 
 }
--- src/com/shum/demoqa/test/navbar/pages/BlogPageElementsTest.java
+++ src/com/shum/demoqa/test/navbar/pages/BlogPageElementsTest.java
@@ -1,9 +1,13 @@
 package com.shum.demoqa.test.navbar.pages;
 
 import org.junit.Test;
+import org.testng.Assert;
 
 import com.shum.demoqa.page.WebPage;
 import com.shum.demoqa.page.navbar.pages.BlogPage;
+import com.shum.demoqa.page.navbar.pages.blog.SamplePostOnePage;
+import com.shum.demoqa.page.navbar.pages.blog.SamplePostTwoPage;
+import com.shum.demoqa.test.navbar.data.pages.BlogPageDataTest;
 import com.shum.demoqa.tests.BaseTest;
 
 public class BlogPageElementsTest extends BaseTest {
@@ -22,8 +26,73 @@ public class BlogPageElementsTest extends BaseTest {
 	public void blogPageTitleTest() {
 		WebPage wPage = new WebPage(wDriver);
 		BlogPage blogPage = wPage.navBar.openBlogPage();
-		
+
 		System.out.println(blogPage.getPageTitle());
 	}
 
+	@Test
+	public void blogPostHeadersTest() {
+		WebPage wPage = new WebPage(wDriver);
+		BlogPage blogPage = wPage.navBar.openBlogPage();
+
+		blogPage.maximizeWindow();
+
+		Assert.assertTrue(blogPage.getSamplePostOneLinkText().equals(BlogPageDataTest.BLOG_PAGE_POST_ONE_HEADER_TEXT),
+				"Fail!. Name of 'Sample Post One' post does not match!.");
+
+		Assert.assertTrue(blogPage.getSamplePostTwoLinkText().equals(BlogPageDataTest.BLOG_PAGE_POST_TWO_HEADER_TEXT),
+				"Fail!. Name of 'Sample Post Two' post does not match!.");
+	}
+
+	@Test
+	public void blogPostDescriptionTextTest() {
+		WebPage wPage = new WebPage(wDriver);
+		BlogPage blogPage = wPage.navBar.openBlogPage();
+
+		Assert.assertTrue(
+				blogPage.getSamplePostOneDescription().equals(BlogPageDataTest.BLOG_PAGE_POST_ONE_DESCRIPTION),
+				"Fail!. Description of 'Sample Post One' post does not match!.");
+
+		Assert.assertTrue(
+				blogPage.getSamplePostTwoDescription().equals(BlogPageDataTest.BLOG_PAGE_POST_TWO_DESCRIPTION),
+				"Fail!. Description of 'Sample Post Two' post does not match!.");
+	}
+
+	@Test
+	public void blogPostOneLinksTest() {
+		WebPage wPage = new WebPage(wDriver);
+		BlogPage blogPage = wPage.navBar.openBlogPage();
+
+		blogPage.maximizeWindow();
+
+		SamplePostOnePage samplePostOnePage;
+
+		samplePostOnePage = blogPage.clickOnSamplePostOne();
+		samplePostOnePage.navigateBack();
+
+		samplePostOnePage = blogPage.clickOnSamplePostOneImagePreview();
+		samplePostOnePage.navigateBack();
+
+		samplePostOnePage = blogPage.clickOnSamplePostOneReadMoreButton();
+		samplePostOnePage.navigateBack();
+	}
+
+	@Test
+	public void blogPostTwoLinksTest() {
+		WebPage wPage = new WebPage(wDriver);
+		BlogPage blogPage = wPage.navBar.openBlogPage();
+
+		blogPage.maximizeWindow();
+
+		SamplePostTwoPage samplePostTwoPage;
+
+		samplePostTwoPage = blogPage.clickOnSamplePostTwo();
+		samplePostTwoPage.navigateBack();
+
+		samplePostTwoPage = blogPage.clickOnSamplePostTwoImagePreview();
+		samplePostTwoPage.navigateBack();
+
+		samplePostTwoPage = blogPage.clickOnSamplePostTwoReadMoreButton();
+		samplePostTwoPage.navigateBack();
+	}
 }
