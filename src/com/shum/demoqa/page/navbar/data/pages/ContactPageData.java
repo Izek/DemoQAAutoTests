8da8b5a91cf4c96b54d295440e5156b8155ac59b
trinetixishum <igor.shumeiko@trinetix.com>
2016-02-25 17:43:33 +0200

    Contact Page locators added.

--- src/com/shum/demoqa/page/navbar/data/pages/ContactPageData.java
+++ src/com/shum/demoqa/page/navbar/data/pages/ContactPageData.java
@@ -2,4 +2,19 @@ package com.shum.demoqa.page.navbar.data.pages;
 
 public interface ContactPageData {
 
+	public final static String CONTACT_PAGE_HEADER_XPATH = "/html/body//main/article/header/h1";
+
+	public final static String CONTACT_PAGE_NAME_INPUT_XPATH = "/html/body//main/article/div/div/form/p[contains(text(), \"Your Name (required)\")]/span/input";
+	public final static String CONTACT_PAGE_EMAIL_INPUT_XPATH = "/html/body//main/article/div/div/form/p[contains(text(), \"Your Email (required)\")]/span/input";
+	public final static String CONTACT_PAGE_SUBJECT_INPUT_XPATH = "/html/body//main/article/div/div/form/p[contains(text(), \"Subject\")]/span/input";
+	public final static String CONTACT_PAGE_MESSAGE_TEXTAREA_XPATH = "/html/body//main/article/div/div/form/p[contains(text(), \"Your Message\")]/span/textarea";
+
+	public final static String CONTACT_PAGE_SEND_BUTTON_XPATH = "/html/body//main/article/div/div/form/p/input[@value=\"Send\"]";
+
+	// SENDING Alerts:
+	public final static String CONTACT_PAGE_NAME_VALIDATION_ERROR_ALERT_XPATH = "/html/body//main/article/div/div/form/p[contains(text(), \"Your Name (required)\")]/span/span[@role=\"alert\"]";
+	public final static String CONTACT_PAGE_EMAIL_VALIDATION_ERROR_ALERT_XPATH = "/html/body//main/article/div/div/form/p[contains(text(), \"Your Email (required)\")]/span/span[@role=\"alert\"]";
+
+	public final static String CONTACT_PAGE_MESSAGE_VALIDATION_ERROR_ALERT_XPATH = "/html/body//main/article/div/div/form/div[@role=\"alert\"]";
+
 }
--- src/com/shum/demoqa/test/navbar/pages/ContactPageElementTest.java
+++ src/com/shum/demoqa/test/navbar/pages/ContactPageElementTest.java
@@ -1,11 +1,36 @@
 package com.shum.demoqa.test.navbar.pages;
 
+import org.junit.Test;
+
+import com.shum.demoqa.page.WebPage;
+import com.shum.demoqa.page.navbar.pages.ContactPage;
 import com.shum.demoqa.tests.BaseTest;
 
 public class ContactPageElementTest extends BaseTest {
 
-	public ContactPageElementTest() {
-		// TODO Auto-generated constructor stub
+	@Override
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Override
+	public void tearDown() {
+		super.tearDown();
+	}
+
+	@Test
+	public void openContactsPageTest() {
+		WebPage webPage = new WebPage(wDriver);
+		webPage.navBar.openHomePage();
+		
+		
+		ContactPage contactPage = webPage.navBar.navigateToContactPage();
+		
+		contactPage.maximizeWindow();
+		
+		contactPage.navigateBack();
+		contactPage.navigateForward();
+		
 	}
 
 }
